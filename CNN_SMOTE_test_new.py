e08e9907c5c4560a9cef6f2278e46701a92a1be9
Yoon Tiem Leong <42529757+tlyoon@users.noreply.github.com>
2025-06-16 11:25:36 +0800

    Add files via upload

--- CNN_SMOTE_test_new.py
+++ CNN_SMOTE_test_new.py
@@ -4,13 +4,20 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from collections import Counter
 
+with open('n_oversample.txt', 'r') as f:
+    content = f.read().strip()
+    n_oversample = int(content)
+print('n_oversample', n_oversample, type(n_oversample))
+#n_oversample = 3000  ### default = 500
+nk_neighbors = 2    #default = 2
+
 Y=np.load('label_test_new.npy',allow_pickle=True)
 X=np.load('config_test_new.npy',allow_pickle=True)
 
 #OVERSAMPLE THE DATASET
 sample_strategy= Counter(Y)#creates dict to get the classes of the label file
-sample_strategy= dict.fromkeys(sample_strategy,500) #sets the number of augmented samples
-sm = SMOTE(sampling_strategy = sample_strategy,  k_neighbors=2) 
+sample_strategy= dict.fromkeys(sample_strategy,n_oversample) #sets the number of augmented samples
+sm = SMOTE(sampling_strategy = sample_strategy,  k_neighbors=nk_neighbors) 
 x_res, y_res = sm.fit_resample(X,Y)
 
 print('Before SMOOTING, number of samples in config_test_new.npy:',X.shape[0])
@@ -21,3 +28,4 @@ np.save("X_test_new",x_res)
 print("File X_test_new.npy has been saved")
 np.save("Y_test_new",y_res)
 print("File Y_test_new.npy has been saved")
+
--- CNN_SMOTE_train_new.py
+++ CNN_SMOTE_train_new.py
@@ -4,12 +4,20 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from collections import Counter
 
+#n_oversample = 500  ### default = 500
+with open('n_oversample.txt', 'r') as f:
+    content = f.read().strip()
+    n_oversample = int(content)
+print('n_oversample', n_oversample, type(n_oversample))
+
+nk_neighbors = 2    #default = 2
+
 Y=np.load('label_train_new.npy',allow_pickle=True)
 X=np.load('config_train_new.npy',allow_pickle=True)
 #OVERSAMPLE THE DATASET
 sample_strategy= Counter(Y)#creates dict to get the classes of the label file
-sample_strategy= dict.fromkeys(sample_strategy,500) #sets the number of augmented samples
-sm = SMOTE(sampling_strategy = sample_strategy,  k_neighbors=2) 
+sample_strategy= dict.fromkeys(sample_strategy,n_oversample) #sets the number of augmented samples
+sm = SMOTE(sampling_strategy = sample_strategy,  k_neighbors=nk_neighbors) 
 x_res, y_res = sm.fit_resample(X,Y)
 np.save("X_train_new",x_res)
 
